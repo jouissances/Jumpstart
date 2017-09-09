6dfcb62f4c2592c07f53837b66cec9257280418e
Janice <janicedarikho@gmail.com>
2017-09-09 17:55:21 +0800

    added all jquery and javascript functionality. need to make responsive.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,33 @@
+/* Slideshow jQuery */
+
 $(document).ready(function(){
-    $('.carousel').slick();
+    $('.jumbotron-carousel').slick({
+          autoplay: true,
+          arrows: false
+    });
+
+/* Scroll jQuery */
+
+var didScroll = 0;
+
+    $(window).scroll(function() {
+        didScroll = true;
+        var st = $(this).scrollTop();
+        
+        if (st - didScroll >= 50) {
+        var navHeight = $('.navbar').css('height');
+        
+        $('.navbar').animate({
+            top: '-' + navHeight}, 150);
+        didScroll = st;
+        }
+        
+        else {
+        $('.navbar').animate({
+            top: '0px'}, "fast");
+        didScroll = st;
+        }
+
+    });
+
 });
